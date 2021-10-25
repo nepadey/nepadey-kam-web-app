71c022d2a97448e463923ea840b618b873b70858
Pratik Shrestha (Sudapey) <60513331+pratiksudapey@users.noreply.github.com>
2021-10-25 20:04:05 +0545

    loginpage designed

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -30,4 +30,8 @@ class FrontendController extends Controller
   {
       return view('frontend.contactpage');
   }
+  public function loginpage()
+  {
+      return view('frontend.loginpage');
+  }
 }
